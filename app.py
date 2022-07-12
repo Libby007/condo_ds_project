e3fb21693106eb6020f330257229bd784aa61aa2
Libby007 <107486104+Libby007@users.noreply.github.com>
2022-07-12 11:14:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient import discovery
 from itertools import chain
 
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+import json
+import random
+from streamlit_chat import message
+
 # menu
 with st.sidebar:
     choose = option_menu("DS Project",
                          ["About", "Toronto condo price prediction", "Condo clustering", "Condo search engine"
-                             , "Multi-criteria ranked condos", "Algorithm theory and principle"],
-                         icons=['fingerprint', 'house fill', 'kanban', 'google', 'award', 'book'],
+                             , "Multi-criteria ranked condos", "Talk to AI Libby", "Algorithm theory and principle"],
+                         icons=['fingerprint', 'house fill', 'kanban', 'google', 'award', 'robot', 'book'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
                              "container": {"padding": "5!important", "background-color": "#fafafa"},
@@ -28,7 +35,6 @@ with st.sidebar:
                              "nav-link-selected": {"background-color": "#02ab21"},
                          }
                          )
-
 if choose == "About":
 
     st.markdown(""" <style> .font {
@@ -676,6 +682,77 @@ elif choose == "Multi-criteria ranked condos":
     else:
         st.error('Please choose weight for your customized multi-criteria!')
 
+elif choose == "Talk to AI Libby":
+    model = load_model('chatbot_model.h5')
+    intents = json.loads(open('intents1.json').read())
+    words = pickle.load(open('words.pkl', 'rb'))
+    classes = pickle.load(open('classes.pkl', 'rb'))
+    lemmatizer = WordNetLemmatizer()
+
+    def clean_up_sentence(sentence):
+        # tokenize the pattern - split words into array
+        sentence_words = nltk.word_tokenize(sentence)
+        # stem each word - create short form for word
+        sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+        return sentence_words
+
+    # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+    def bow(sentence, words, show_details=True):
+        # tokenize the pattern
+        sentence_words = clean_up_sentence(sentence)
+        # bag of words - matrix of N words, vocabulary matrix
+        bag = [0] * len(words)
+        for s in sentence_words:
+            for i, w in enumerate(words):
+                if w == s:
+                    # assign 1 if current word is in the vocabulary position
+                    bag[i] = 1
+                    if show_details:
+                        print("found in bag: %s" % w)
+        return (np.array(bag))
+    def predict_class(sentence, model):
+        # filter out predictions below a threshold
+        p = bow(sentence, words, show_details=False)
+        res = model.predict(np.array([p]))[0]
+        ERROR_THRESHOLD = 0.25
+        results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+        # sort by strength of probability
+        results.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
+        for r in results:
+            return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+        return return_list
+    def getResponse(ints, intents_json):
+        tag = ints[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if (i['tag'] == tag):
+                result = random.choice(i['responses'])
+                break
+        return result
+    def chatbot_response(msg):
+        ints = predict_class(msg, model)
+        res = getResponse(ints, intents)
+        return res
+    def get_text():
+        input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+        return input_text
+
+    st.header("Chat with AI to know more about Libby")
+    user_input = get_text()
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+    if user_input:
+        output = chatbot_response(user_input)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output)
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated']) - 1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 elif choose == "Algorithm theory and principle":
 
