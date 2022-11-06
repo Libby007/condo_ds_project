edb870cf5c4f4eda99cf236ef7f87222cd47a2a3
Libby007 <107486104+Libby007@users.noreply.github.com>
2022-11-05 20:09:07 -0400

    Update chatbot_main_file.py

--- code/chatbot_main_file.py
+++ code/chatbot_main_file.py
@@ -77,11 +77,11 @@ print("Training data created")
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu')) #split into 128 dimensional
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu')) #split into 64 dimensional
 model.add(Dropout(0.3))
-model.add(Dense(len(train_y[0]), activation='softmax'))
+model.add(Dense(len(train_y[0]), activation='softmax')) #output layer
 
 # # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
